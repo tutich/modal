1be741885e9f375ba2cf9a1b9f21e177c02ac735
tutich <pkoinange02@gmail.com>
2023-09-06 22:26:58 +0300

    html structure

--- modal.js
+++ modal.js
@@ -1,11 +1,11 @@
-const openBtn = document.querySelector(".open-btn");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelectorAll(".close-btn");
+// const openBtn = document.querySelector(".open-btn");
+// const overlay = document.querySelector(".overlay");
+// const closeBtn = document.querySelectorAll(".close-btn");
 
-openBtn.addEventListener("click", function() {
-    overlay.classList.add("open-modal");
-});
+// openBtn.addEventListener("click", function() {
+//     overlay.classList.add("open-modal");
+// });
 
-closeBtnBtn.addEventListener("click", function() {
-    overlay.classList.remove("open-modal");
-});
+// closeBtnBtn.addEventListener("click", function() {
+//     overlay.classList.remove("open-modal");
+// });
